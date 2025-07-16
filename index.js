5997ffb7c877cd4f5cb1e267d4ff7af948817f28
Lakshmi <skshrivastavshrivastav36@gmail.com>
2025-07-16 19:32:39 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>JavaScript Button Example</title>
+</head>
+<body>
+
+  <h2>Click the Button Below</h2>
+  <button onclick="showMessage()">Click Me</button>
+
+  <script>
+    function showMessage() {
+      alert("Hello! You clicked the button.");
+    }
+  </script>
+
+</body>
+</html>
